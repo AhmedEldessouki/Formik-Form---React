28628fba1187f68b681d84aee6ea13e423965e37
AhmedEldessouki <44158955+AhmedEldessouki@users.noreply.github.com>
2018-11-29 17:29:27 +0300

    no message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
+import FormikForm from './component/FormikForm/FormikForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <FormikForm />
         </header>
       </div>
     );
