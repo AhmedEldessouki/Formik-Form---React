77a4ac3d6767345526238391f9c4c976e7ae6db6
AhmedEldessouki <44158955+AhmedEldessouki@users.noreply.github.com>
2018-12-02 11:12:58 +0300

    sample of a formik form no css

--- src/component/FormikForm/FormikForm.js
+++ src/component/FormikForm/FormikForm.js
@@ -1,31 +1,60 @@
-import React, { Component } from 'react'
-// import { Formik } from 'formik';
-// import * as Yup from 'yup';
-import { Form, Icon, Input, Button } from 'antd';
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-const FormItem = Form.Item;
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+});
 
-export default class FormikForm extends Component {
-  render() {
-    return (
-      <div className="FormikForm">
-        <Form layout="inline" onSubmit={this.handleSubmit}>
-          <FormItem>
-            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-          </FormItem>
-          <FormItem>
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-          </FormItem>
-          <FormItem>
-            <Button
-              type="primary"
-              htmlType="submit"
-            >
-              Log in
-            </Button>
-          </FormItem>
-        </Form>
-      </div>
-    )
+function validateName(value) {
+  let error;
+  if (value === 'admin'|| 'god') {
+    error = 'Nice try!';
   }
+  return error;
 }
+
+ const FormikForm = () => (
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={values => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field name="firstName"  validate={validateName}/>
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <Field name="lastName" validate={validateName}/>
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <Field name="email" type="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+export default FormikForm
